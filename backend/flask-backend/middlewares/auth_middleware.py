06b1824bfbb98a933da5920bfdfd6218ea153e34
Akash Jadhav <142928804+Akash1510@users.noreply.github.com>
2026-04-14 16:49:49 +0530

    change code

--- backend/flask-backend/middlewares/auth_middleware.py
+++ backend/flask-backend/middlewares/auth_middleware.py
@@ -5,7 +5,7 @@ from flask import request,jsonify,g
 
 JWT_SECRET = os.getenv("JWT_SECRET","littleboy@@7776032964")
 JWT_ALGORITHM="HS256"
-
+print("JWT Secret in Middleware:", JWT_SECRET);
 def verify_token(required_role=None):
 
     def decorator(f):
