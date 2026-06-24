b84cecc7e86adee7641e3c11374ecc4db763f779
Akash Jadhav <142928804+Akash1510@users.noreply.github.com>
2026-06-24 19:45:02 +0530

    fix: Redis connectivity Update

--- backend/flask-backend/db/cache.py
+++ backend/flask-backend/db/cache.py
@@ -2,11 +2,11 @@ import redis
 import json
 
 redis_client = redis.Redis(
-    host='redis-10362.c326.us-east-1-3.ec2.cloud.redislabs.com',
-    port=10362,
+    host='cat-dad-yarn-12166.db.redis.io',
+    port=18547,
     db=0,
     decode_responses=True,
-    password='d2Os6yFL48CT7nEKnn5z0gHXZFtKNgbS'
+    password='ezftYsET8sFM5nbNHCCmZMYCvSZOFgGF'
 )
 
 
